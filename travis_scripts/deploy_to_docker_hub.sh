e16b93276cc077fb127e05c68ba11b5aa3c6a7f8
cjp <cjpthree@126.com>
2018-11-08 20:38:21 +0800

    do travis

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker push cjpthree/tmx-authentication-service:$BUILD_NAME
+docker push cjpthree/tmx-licensing-service:$BUILD_NAME
+docker push cjpthree/tmx-organization-service:$BUILD_NAME
+docker push cjpthree/tmx-confsvr:$BUILD_NAME
+docker push cjpthree/tmx-eurekasvr:$BUILD_NAME
+docker push cjpthree/tmx-zuulsvr:$BUILD_NAME
